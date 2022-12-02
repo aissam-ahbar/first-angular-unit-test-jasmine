caa2d4984cbfe341b95085ffa606ab1cfa0e1e4a
aissam-ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2022-12-02 23:34:17 +0100

    1 file modified

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,24 +2,20 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  // ARRANGE STEP : Setup dependencies & create your component (TestBed).
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
     }).compileComponents();
   });
 
-  it(`given null or undefined name, should return just Hello'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    const name: string;
-    const actual = app.displayHello(name);
-    expect(actual).toEqual('Hello Aissam');
-  });
-
   it(`given valid name, should return Hello with name'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    const actual = app.displayHello('Aissam');
-    expect(actual).toEqual('Hello Aissam');
+
+    // ASSERT STEP: Check your code matches your expectations
+    const expected = 'Hello Aissam';
+    const res = app.displayHello(expected);
+    expect(res).toEqual(expected);
   });
 });
